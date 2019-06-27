9dd55e97f0702f11801b76b8db93dcece85726f0
Ilori Bolutife <iloriboluwatife@gmail.com>
2019-06-27 10:49:42 +0100

    Assignment

---
+++ First_code.py
@@ -0,0 +1,54 @@
+# # print ("hello world")
+# first_name = "Bolutife"
+# last_name = "Ilori"
+# # age = "22"
+# # origin = "Nigeria"
+# # statement = f"{first_name} {last_name} is {age} years old and comes from {origin}"
+# # print(statement)
+# product1 = "apple"
+# product2 = "banana"
+# buyer = f"{first_name} {last_name}"
+# price1 = 200
+# price2 = 300
+# quantity1 = 4
+# quantity2 = 6
+# total1 = price1 * quantity1
+# total2 = price2 * quantity2
+# statement = f"Hello {buyer}, you have purchased {quantity1} {product1} at {price1} each and {quantity2} {product2} at {price2} each and your total is {total1} and {total2} respectively"
+# print(statement)
+# var1 = input("enter first number: ")
+# var2 = input("enter second number: ")
+# var3 = input("enter third number: ")
+# sum_of_var = int(var1) + int(var2) + int(var3)
+# print(sum_of_var)
+
+# product = input ("what do you want to buy? ")
+# quantity = input ("how many do you want to purchase? ")
+# price = input ("what is the price? ")
+# buyer = input ("what is your name? ")
+# total = int(price)* int(quantity)
+# statement = f" Hello {buyer}, you have purchased {quantity} {product} at {price} each and total is {total}"
+# print(statement)
+
+# radius = input(" what is the radius? ")
+# pie = 3.142
+# circumference = 2 *pie*int(radius)
+# print (round(circumference))
+# statement = f" the circumference of a circle with radius {radius} is {circumference}"
+# print(statement)
+# 
+# pass_mark = 70
+# score = int(input("what is your score? "))
+
+# response = f"Pass : {score >= pass_mark}"
+# print(response) 
+
+text = input ("Say anything: ")
+value = input ("Any word in text: ")
+statement = f" Word in text : {value in text}"
+print(statement)
+
+experiment = input ("Word: ")
+sample = experiment[::-1]
+sentence = f" Palindrome {sample == experiment}"
+print(sentence)
